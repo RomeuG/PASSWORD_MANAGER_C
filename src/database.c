dc3cd58bc7d28ec3659b2f90468dbf745ad6b977
Romeu Vieira <romeu.bizz@gmail.com>
2018-08-16 23:33:27 +0100

    simplify database functions

--- src/database.c
+++ src/database.c
@@ -4,8 +4,6 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-sqlite3 *db;
-
 bool sql3_db_exists(char *db_name)
 {
 	// struct stat   buffer;   
@@ -21,27 +19,11 @@ bool sql3_db_exists(char *db_name)
 // database open encapsulation
 int sql3_db_init(sqlite3 *_db, char *db_name) 
 {
-    int error_check;
-    
-    error_check = sqlite3_open_v2(db_name, &_db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
-
-    if(error_check != SQLITE_OK) {
-        return error_check;
-    }
-
-    return SQLITE_OK;
+    return sqlite3_open_v2(db_name, &_db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
 }
 
-// database close
+// database close encapsulation
 int sql3_db_close(sqlite3 *_db) 
 {
-    int error_check;
-
-    error_check = sqlite3_close_v2(_db);
-
-    if(error_check != SQLITE_OK) {
-        return error_check;
-    }
-
-    return SQLITE_OK;
+    return sqlite3_close_v2(_db);
 }
