b4587871ed0dd4ad0628f5b80b4ab4bc6a2274cc
Romeu Vieira <romeu.bizz@gmail.com>
2018-08-18 18:27:40 +0100

    main.c changed for tests

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char** argv) 
 {
-    char* database_name;
+    u8 key[AES_KEY_LENGTH] = {'t', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 't'};
+    u8 iv[AES_BLOCK_SIZE] = {'t', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 't', 't', 'e', 's', 's'};
+
 
     if(argc != 2) {
         _Exit(EXIT_FAILURE);
