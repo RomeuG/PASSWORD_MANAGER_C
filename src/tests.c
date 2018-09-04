d6791366a94b88e71e38d8b6616baffb7aa0e3de
Romeu Vieira <romeu.bizz@gmail.com>
2018-09-04 23:01:14 +0100

    First test being ran with success

--- src/tests.c
+++ src/tests.c
@@ -1,8 +1,17 @@
 #include "tests.h"
 
-#include "encryption.h"
-
 void __tests()
 {
+    char pass[] = "random_password";
+    unsigned char* salt = "12345678";
 
+    char *result = malloc(2 * PBKDF2_OUTPUT_SIZE);
+
+    bool res = PBKDF2_HMAC_SHA_X(pass, sizeof(pass), salt, PKCS5_SALT_LEN, result);
+    if (!res) {
+        DEBUG_PRINT("%s\n", "test failed");
+        return;
+    }
+
+    printf("Hex result: %s\n", result);
 }
--- src/tests.h
+++ src/tests.h
@@ -1,6 +1,9 @@
 #ifndef __TESTS_H
 #define __TESTS_H
 
+#include "utils.h"
+#include "encryption.h"
+
 void __tests();
 
 #endif
