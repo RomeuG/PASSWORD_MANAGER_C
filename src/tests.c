037dfccf89c517f16a1b128bbff4627eae35bca5
Romeu Vieira <romeu.bizz@gmail.com>
2018-09-05 22:24:30 +0100

    Macro for assertions.

--- src/tests.c
+++ src/tests.c
@@ -1,6 +1,13 @@
+#include <assert.h>
 #include "tests.h"
 
-void __tests()
+#define assert__(function, operation, expected) { \
+    assert(function() operation expected);\
+}
+
+u8 i;
+
+bool __test_pbkdf2_hmac_sha1()
 {
     char pass[] = "random_password";
     unsigned char* salt = "12345678";
@@ -10,8 +17,14 @@ void __tests()
     bool res = PBKDF2_HMAC_SHA_X(pass, sizeof(pass), salt, PKCS5_SALT_LEN, result);
     if (!res) {
         DEBUG_PRINT("%s\n", "test failed");
-        return;
+        return false;
     }
 
     printf("Hex result: %s\n", result);
+    return true;
+}
+
+void __tests()
+{
+    assert__(__test_pbkdf2_hmac_sha1, CMP_E, true);
 }
