14c552fb6bc07ddbee58795e37bd73f9ff2ce014
Romeu Vieira <romeu.bizz@gmail.com>
2018-09-07 00:09:45 +0100

    Refactoring + fixes to support more than 1 test

--- src/tests.c
+++ src/tests.c
@@ -1,51 +1,9 @@
 #include <assert.h>
 #include "tests.h"
 
-#define _ASSERT(function, operation, expected) { \
-    if (strcmp(operation, CMP_E_STR) == 0) {\
-        __ASSERT(function() CMP_E expected);\
-    } else if (strcmp(operation, CMP_NE_STR) == 0) { \
-        __ASSERT(function() CMP_NE expected);\
-    } else if (strcmp(operation, CMP_G_STR) == 0) { \
-        __ASSERT(function() CMP_G expected);\
-    } else if (strcmp(operation, CMP_GE_STR) == 0) { \
-        __ASSERT(function() CMP_GE expected);\
-    } else if (strcmp(operation, CMP_L_STR) == 0) { \
-        __ASSERT(function() CMP_L expected);\
-    } else if (strcmp(operation, CMP_LE_STR) == 0) { \
-        __ASSERT(function() CMP_LE expected);\
-    } \
-}
-
-#define REGISTER_TEST(_function, _operation, _expectation) { \
-     jump_table[0].function = _function;\
-     strcpy(jump_table[0].operation, _operation);\
-     jump_table[0].expectation = _expectation;\
-}
-
-#define ASSERT_ALL() {\
-    for (int j = 0; j < 1; j++) { \
-        _ASSERT(jump_table[j].function, jump_table[j].operation, jump_table[j].expectation); \
-    } \
-}
-
-#define __ASSERT(expr)							\
-  ((void) sizeof ((expr) ? 1 : 0), __extension__ ({			\
-      if (expr)								\
-        i++; /* TODO */							\
-      else								\
-        __assert_fail (#expr, __FILE__, __LINE__, __ASSERT_FUNCTION);	\
-    }))
-
-
-struct __test_case {
-	bool (*function)();
-	char operation[3];
-	bool expectation;
-};
-
-u8 i = 0;
-struct __test_case jump_table[32];
+u8 successful_tests = 0;
+u8 tests_registered = 0;
+struct __test_case jump_table[TEST_NUMBER];
 
 bool __test_pbkdf2_hmac_sha1()
 {
@@ -64,10 +22,16 @@ bool __test_pbkdf2_hmac_sha1()
     return true;
 }
 
+bool _test2()
+{
+    return true;
+}
+
 void __tests()
 {
 	REGISTER_TEST(__test_pbkdf2_hmac_sha1, CMP_E_STR, true);
+	REGISTER_TEST(_test2, CMP_E_STR, true);
     ASSERT_ALL();
 
-    printf("%d out of %d tests complete!\n", i, i);
+    printf("%d out of %d tests complete!\n", successful_tests, TEST_NUMBER);
 }
--- src/tests.h
+++ src/tests.h
@@ -4,6 +4,52 @@
 #include "utils.h"
 #include "encryption.h"
 
+#define TEST_NUMBER 2
+
+#define _ASSERT(function, operation, expected) { \
+    if (strcmp(operation, CMP_E_STR) == 0) {\
+        __ASSERT(function() CMP_E expected);\
+    } else if (strcmp(operation, CMP_NE_STR) == 0) { \
+        __ASSERT(function() CMP_NE expected);\
+    } else if (strcmp(operation, CMP_G_STR) == 0) { \
+        __ASSERT(function() CMP_G expected);\
+    } else if (strcmp(operation, CMP_GE_STR) == 0) { \
+        __ASSERT(function() CMP_GE expected);\
+    } else if (strcmp(operation, CMP_L_STR) == 0) { \
+        __ASSERT(function() CMP_L expected);\
+    } else if (strcmp(operation, CMP_LE_STR) == 0) { \
+        __ASSERT(function() CMP_LE expected);\
+    } \
+}
+
+#define REGISTER_TEST(_function, _operation, _expectation) { \
+     jump_table[tests_registered].function = _function;\
+     strcpy(jump_table[tests_registered].operation, _operation);\
+     jump_table[tests_registered].expectation = _expectation;\
+     tests_registered++; \
+}
+
+#define ASSERT_ALL() {\
+    for (int j = 0; j < TEST_NUMBER; j++) { \
+        _ASSERT(jump_table[j].function, jump_table[j].operation, jump_table[j].expectation); \
+    } \
+}
+
+#define __ASSERT(expr)							\
+  ((void) sizeof ((expr) ? 1 : 0), __extension__ ({			\
+      if (expr)								\
+        successful_tests++; /* TODO */							\
+      else								\
+        __assert_fail (#expr, __FILE__, __LINE__, __ASSERT_FUNCTION);	\
+    }))
+
+
+struct __test_case {
+	bool (*function)();
+	char operation[3];
+	bool expectation;
+};
+
 void __tests();
 
 #endif
