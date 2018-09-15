e87a1ab4fbbc74b39b0817f2506d4726830e60d4
Romeu Vieira <romeu.bizz@gmail.com>
2018-09-15 15:54:53 +0100

    Solve major bug. Encryption module needs to work with larger keys

--- src/encryption.c
+++ src/encryption.c
@@ -1,6 +1,15 @@
 #include "encryption.h"
 #include "utils.h"
 
+void SSL_print_error()
+{
+	char *err = malloc(128);
+	ERR_load_crypto_strings();
+	ERR_error_string(ERR_get_error(), err);
+	DEBUG_PRINT(stderr, "%s\n", err);
+	free(err);
+}
+
 bool PBKDF2_HMAC_SHA_X(char* pass, u8 pass_len, unsigned char* salt, u8 salt_len, char* hex_result)
 {
 	u32 i;
@@ -28,17 +37,22 @@ bool PBKDF2_HMAC_SHA_X(char* pass, u8 pass_len, unsigned char* salt, u8 salt_len
 s32 _AES_CBC_encrypt(u8 *message, u8 *buffer_out, size_t length, u8 *key, u8 *iv_enc)
 {
 	s32 res;
+	u8 new_key[AES_BLOCK_SIZE];
+	// output size
+	size_t out_size = ((length/AES_BLOCK_SIZE) + 1) * AES_BLOCK_SIZE;
+
+	// copy 16 bytes of the 64
+	memcpy(new_key, key, AES_BLOCK_SIZE);
 
 	AES_KEY encryption_key;
-	res = AES_set_encrypt_key(key, AES_KEY_LENGTH * 8, &encryption_key);
+	res = AES_set_encrypt_key(new_key, AES_BLOCK_SIZE * 8, &encryption_key);
 
 	if (res < 0) {
-		printf("ERROR");
+		SSL_print_error();
 		return res;
 	}
 
-	AES_cbc_encrypt(message, buffer_out, length, &encryption_key, iv_enc, AES_ENCRYPT);
-
+	AES_cbc_encrypt(message, buffer_out, out_size, &encryption_key, iv_enc, AES_ENCRYPT);
 	return res;
 }
 
@@ -50,7 +64,7 @@ s32 _AES_CBC_decrypt(u8 *message, u8 *buffer_out, size_t length, u8 *key, u8 *iv
 	res = AES_set_decrypt_key(key, AES_KEY_LENGTH * 8, &decryption_key);
 
 	if (res < 0) {
-		printf("ERROR");
+		DEBUG_PRINT(stderr, "%s\n", "ERROR");
 		return res;
 	}
 
